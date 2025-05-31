5b2b4e4baaff8eefc7c279912155010a8d3a1f67
edahigure <edahigure@gmail.com>
2025-05-30 20:47:55 -0500

    Create mix.sh

---
+++ mix.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+
+./bible luke    1  1  4
+./bible john    1  1  14
+./bible matthew 1  1  17 
+./bible luke    3  23 38
+./bible matthew 1  18 23
+./bible luke    1  5  80
+./bible luke    2  1  39
+./bible matthew 2  1  10
+./bible matthew 2  11 15
+./bible matthew 2  16 18
+./bible matthew 2  19 23
+./bible luke    2  40 52
+
+./bible luke    3  1  6
+./bible mark    1  1  8
+./bible matthew 3  1  6
+
+
+./bible matthew 3  7  12
+./bible luke    3  7  14  
+
+
+
+./bible luke    3  15 18 
+./bible john    1  15 18
+
+
+./bible mark    1  9  11
+
+
+./bible luke    3  21 23 
+
+./bible john    1  19 28
+./bible john    1  29 34
+./bible john    1  35 42
+./bible john    1  43 51
+./bible john    2  1  11
+./bible john    2  12
+./bible john    2  13 17
+./bible john    2  18 22
+./bible john    2  23 25    
+
+
+./bible john    3
+./bible john    4
+./bible john    5
+
+./bible luke    3  19  20   #Juan apresado
+
+./bible mark    1  12  13
+./bible matthew 4  1   11
+./bible luke    4  1   13   #Jesus en el desierto
+
+
+./bible luke    4  14  15   #Jesus in Galilea
+
+./bible luke    4  16  30   #Jesus in Nazaret
+
+
+./bible mark    1  14  15
+./bible matthew 4  12  16  #Jesus oye muerte de Juan y se va
+./bible matthew 4  17  
+
+./bible matthew 4  18  22 
+./bible mark    1  16  20  #llamamiento dicipulos de
+
+./bible matthew 4  23  25  #Jesus en capernaum resumen
+
+
+
+./bible mark    1  21  28  
+./bible luke    4  31  37  #libera endemoniado
+
+
+ 
+./bible mark    1  29  31
+./bible luke    4  38  39  #Jesus sana suegra de pedro
+./bible matthew 8  14 15	#Jesus sana la suegra de Pedro
+
+
+
+./bible mark    1  32  34
+./bible luke    4  40  41 #Sanidad a la puesta de sol
+
+
+
+./bible mark    1  35  38
+./bible luke    4  42  44 #Lo encuentran en la mañana
+
+
+./bible luke    5  1  3  #Predica en el bote
+
+./bible luke    5  4  11 #Pesca milagrosa
+
+
+
+
+./bible matthew 8  1  4 
+./bible mark    1  39  45  #Jesus sana a un leproso
+./bible luke    5  12  16
+
+
+
+
+./bible mark    2  1  12  #Jesus sana paralitico
+./bible luke    5  17  26 
+
+
+./bible luke    5  27  28    
+./bible mark    2  13 14    #llamamiento de Levi
+
+
+
+./bible mark    2  15  22  #Fiesta de Levi
+./bible luke    5  29  39 
+
+
+./bible mark    2  23  28  #Problema del sabado
+./bible luke    6  1   5
+
+
+./bible mark    3  1   6  #Jesus sana mano seca en sabado
+./bible luke    6  6   11
+
+./bible mark    3  7   12 #Jesus se va al mar
+
+
+./bible mark    3  13   19  #Jesus sube la montaña
+./bible luke    6  12   16  #Jesus llama sus dicipulos 
+
+
+./bible matthew 5
+./bible matthew 6
+./bible matthew 7
+
+
+
+./bible mark    3  20  21  #multitud y Sus amigos vienen por él
+
+./bible mark    3  22  30  #Blasfemia contra el espiritu santo
+
+./bible mark    3  31   35  #vienen su madre y sus hermanos
+
+
+./bible matthew 8  1  4    #Jesus sana leproso bajando de la montaña
+
+./bible luke    6  17   19  #Jesus baja de la montaña
+
+
+./bible luke    6  20   49  # Sermon de la planicie
+
+
+./bible mark    4  1   2    #Jesus enseña desde la barca
+./bible mark    4  3   9    #parabola del sembrador  
+./bible mark    4  10  13  #pregunta de la parabola
+./bible mark    4  14  20  #explicacion
+./bible mark    4  21  23  #donde poner la lampara
+./bible mark    4  24  25  #como escuchar
+./bible mark    4  26  29  #reino de Dios como semilla en el campo
+./bible mark    4  30  34  #reino de Dios como semilla de moztaza
+
+
+
+./bible luke 	 7  1  10   #capernaum Jesus sana siervo de centurion
+./bible matthew 8  5  13   #capernaum Jesus sana siervo de centurion
+
+
+./bible luke 	 7  11  18  #Nain Jesus resucita muerto
+./bible luke 	 7  19 23   #Juan manda dicipulos a Jesus 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
